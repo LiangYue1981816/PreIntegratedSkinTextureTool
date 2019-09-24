0271116a427b6ac5f2451aed0d1044c7e77caa48
yueliang <yueliang@tencent.com>
2019-09-24 16:01:47 +0800

    + 添加ToneMapping

--- PreIntegratedSkinTextureTool/PreIntegratedSkinTextureTool.cpp
+++ PreIntegratedSkinTextureTool/PreIntegratedSkinTextureTool.cpp
@@ -25,6 +25,17 @@ glm::vec3 Linear2Gamma(glm::vec3 linear)
 	return glm::pow(linear, glm::vec3(1.0f / 2.2f));
 }
 
+glm::vec3 ToneMapping(glm::vec3 color)
+{
+	// https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+	float a = 2.51f;
+	float b = 0.03f;
+	float c = 2.43f;
+	float d = 0.59f;
+	float e = 0.14f;
+	return glm::clamp((color * (a * color + b)) / (color * (c * color + d) + e), glm::vec3(0.0f), glm::vec3(1.0f));
+}
+
 float Gaussian(float v, float r)
 {
 	return 1.0f / sqrt(2.0f * PI * v) * exp(-(r * r) / (2.0f * v));
